82aacde838462c01c949d16b9bf6bd90925b4dbc
jaxongir <example@gmail.com>
2024-12-10 20:02:03 +0500

    first commit

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+   <h2 class="font-medium text-green">biugkbugvk</h2>
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+     colors: {
+       blue: '#3490dc',
+       green: '#4CAF50',
+       red: '#f44336',
+       black: '#000',
+       white: '#fff',
+     },
+    },
+  },
+  plugins: [],
+}
+
